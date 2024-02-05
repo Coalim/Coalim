640adbd71f5bc349c6db6de17af5b4fa1a023e0c
jvyden <jvyden@jvyden.xyz>
2024-02-05 18:26:03 -0500

    Set max username length to 24

--- Coalim.Database.Schema/Data/CoalimUser.cs
+++ Coalim.Database.Schema/Data/CoalimUser.cs
@@ -7,6 +7,6 @@ public class CoalimUser
     [Key, ExcludeFromCodeCoverage]
     public Guid UserId { get; init; }
     
-    [Required, DataType(DataType.Text)]
+    [Required, DataType(DataType.Text), MaxLength(24)]
     public string Username { get; set; }
 }
